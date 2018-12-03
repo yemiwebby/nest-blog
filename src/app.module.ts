21a07b44b072f3b6e955a1ff4a39ff5c632c7fd1
yemiwebby <yemiwebby@gmail.com>
2018-12-03 04:57:25 +0100

    added files for module and others update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest-blog', { useNewUrlParser: true }),
+    BlogModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
